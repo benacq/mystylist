cf2bb9ad09f9d34404a8f54a4dbc8c8436ed61d7
benacq <benacq44@gmail.com>
2020-11-01 15:51:18 +0000

    reduced image size and did some clean up

---
+++ lib/utils/message_consts.dart
@@ -0,0 +1,20 @@
+library constants;
+
+const String SUCCESS_MESSAGE = " You will be contacted by us very soon.";
+
+const String TIMEOUT_MESSAGE =
+    "Please make sure you have a stable internet connection";
+const String TIMEOUT_TITLE = "REQUEST TIMEOUT";
+
+const String USER_NOT_FOUND_MESSAGE =
+    "This credentials do not match with any user in our records";
+
+const String WRONG_CREDENTIALS_MESSAGE = "Your email or password is incorrect";
+
+const String TOO_MANY_REQUESTS_MESSAGE =
+    "We have blocked all requests from this device due to unusual activity. Try again later.";
+const String TOO_MANY_REQUESTS_TITLE = "TOO MANY REQUESTS";
+
+const String NO_CONNECTION_MESSAGE =
+    "Failed to establish connection, please ensure you have a stable connection and try again";
+const String NO_CONNECTION_TITLE = "CONNECTION FAILED";
